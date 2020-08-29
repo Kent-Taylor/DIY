dc9e7232daaa0e0e6b9b95fabfb30c7f60eedd1a
107124 <kredfox4@gmail.com>
2020-08-28 23:10:40 -0400

    first initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import router from "./utils/router";
+import { createAppContainer } from "react-navigation";
+const AppContainer = createAppContainer(router);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppContainer />
     </View>
   );
 }
@@ -14,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
