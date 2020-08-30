6cf2cf4e2f37cea3eec3e76b849f9fba10f58d43
107124 <kredfox4@gmail.com>
2020-08-30 16:32:47 -0400

    added a search screen and stackable navigation. Created an interface function for better coding experience throughout the rest of the app building process.

--- App.tsx
+++ App.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
 
 import router from "./utils/router";
 import { createAppContainer } from "react-navigation";
 const AppContainer = createAppContainer(router);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <AppContainer />
-    </View>
-  );
+  return <AppContainer />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#C97373",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+
--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
-export default () => {
+interface IFeedScreenProps {
+    navigation: {
+        navigate: (arg: string) => void;
+    }
+}
+export default (props: IFeedScreenProps) => {
     return (
         <View>
             <Text>Feed Screen</Text>
+
+            <TouchableOpacity onPress={() => props.navigation.navigate("Search")}>
+                <Text>
+                    search
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 };
