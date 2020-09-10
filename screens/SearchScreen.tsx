e4063aee4de59e7aafafb617dbbb7ab184b7a8f1
107124 <kredfox4@gmail.com>
2020-09-10 11:30:22 -0400

    finished the now working search bar and search engine. Added styles to it as well.

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,6 +1,23 @@
 import React, { useState } from "react";
-import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import {
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    ActivityIndicator,
+} from "react-native";
+import * as SecureStore from "expo-secure-store";
 import Container from "../components/layouts/Container";
+import api from "../utils/api";
+import PostList from "../components/posts/PostList";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+import searchStyles from "../styles/stacks/posts/searchStyles";
+const {
+    searchFormContainer,
+    searchTextInput,
+    searchIcon
+} = searchStyles;
 
 interface ISearchScreenProps {
     navigation: {
@@ -9,31 +26,53 @@ interface ISearchScreenProps {
 }
 export default (props: ISearchScreenProps) => {
     const [query, setQuery] = useState("");
+    const [posts, setPosts] = useState([]);
 
-    const handleSearch = () => {
-        console.log("Searching for", query);
+    const handleSearch = async () => {
+        const token = await SecureStore.getItemAsync("memipedia_secure_token");
+
+        const params = {
+            query,
+        };
+
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        api
+            .get("memipedia_queries", {
+                params,
+                headers,
+            })
+            .then((response) => {
+                console.log("response from query", response.data);
+                setPosts(response.data.memipedia_posts)
+            })
+            .catch((error) => {
+                console.log("Error running query", error);
+            });
     };
 
     const searchBar = (
-        <View>
+        <View style={searchFormContainer}>
             <TextInput
+                style={searchTextInput}
                 value={query}
                 onChangeText={(val) => setQuery(val)}
-                placeholderTextColor="white"
                 placeholder="Search for a post"
                 onSubmitEditing={handleSearch}
             />
 
-            <TouchableOpacity style={{ marginTop: 20 }} onPress={handleSearch}>
-                <Text style={{ color: "white" }}>Search</Text>
+            <TouchableOpacity style={searchIcon} onPress={handleSearch}>
+                <Ionicons name="md-search" color="white" size={30} />
             </TouchableOpacity>
         </View>
     );
 
     return (
         <Container navigate={props.navigation.navigate}>
-            <Text>SearchScreen</Text>
             {searchBar}
+            <PostList posts={posts} navigate={props.navigation.navigate} />
         </Container>
     );
 };
